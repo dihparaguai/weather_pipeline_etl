4bee51928977c4d681e7a9566773838a224fd37c
dihparaguai <diego.paraguai.dp@gmail.com>
2026-04-15 09:45:13 -0300

    test: add unit tests for get_max_date_from_db

--- tests/test_load.py
+++ tests/test_load.py
@@ -7,7 +7,41 @@ from src.jobs.load import get_max_date_from_db
 class FakeEngine:
     pass
 
-def test_get_max_date_from_db_returns_date_when_valid(monkeypatch):
+def test_get_max_date_from_db_returns_error_when_table_not_exists(monkeypatch):
+    """
+    CENÁRIO: Valida se a função retorna erro quando a tabela não existir.
+    """
+    # Arrange
+    class MockInspector:
+        def has_table(self, table_name):
+            return False
+    
+    # Injeção de dependências
+    monkeypatch.setattr("src.jobs.load.inspect", lambda engine: MockInspector())
+    
+    # Act and Assert
+    with pytest.raises(ValueError):
+        get_max_date_from_db(db_engine=FakeEngine(), table_name='tb_weather_data', column_name='datetime')
+
+def test_get_max_date_from_db_returns_error_when_column_not_exists(monkeypatch):
+    """
+    CENÁRIO: Valida se a função retorna erro quando a coluna não existir.
+    """
+
+    # Arrange 
+    class MockInspector:
+        def has_table(self, table_name):
+            return True
+        def get_columns(self, table_name):
+            return [{'name': 'date'}]
+    
+    monkeypatch.setattr("src.jobs.load.inspect", lambda engine: MockInspector())
+
+    # Act and Assert
+    with pytest.raises(ValueError):
+        get_max_date_from_db(db_engine=FakeEngine(), table_name='tb_weather_data', column_name='datetime')
+
+def test_get_max_date_from_db_returns_date_when_exists(monkeypatch):
     """
     CENÁRIO: Valida se a função retorna a data máxima corretamente.
     """
@@ -31,5 +65,29 @@ def test_get_max_date_from_db_returns_date_when_valid(monkeypatch):
     result = get_max_date_from_db(db_engine=FakeEngine(), table_name='tb_weather_data', column_name='datetime')
 
     # Assert: Validação
-    assert result is not None
     assert str(result) == '2026-04-12'
+
+
+def test_get_max_date_from_db_returns_none_when_table_empty(monkeypatch):
+    """
+    CENÁRIO: Valida se a função retorna None quando a query SQL iterar tabela vazia.
+    """
+    # Arrange
+    def mock_read_sql_empty(query, con):
+        return pd.DataFrame({'max_date': [None]})
+
+    class MockInspector:
+        def has_table(self, table_name):
+            return True
+        def get_columns(self, table_name):
+            return [{'name': 'datetime'}]
+
+    # Injeção de dependências
+    monkeypatch.setattr("src.jobs.load.pd.read_sql", mock_read_sql_empty)
+    monkeypatch.setattr("src.jobs.load.inspect", lambda engine: MockInspector())
+
+    # Act
+    result = get_max_date_from_db(db_engine=FakeEngine(), table_name='tb_weather_data', column_name='datetime')
+
+    # Assert
+    assert result is None
