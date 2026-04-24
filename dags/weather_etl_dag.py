e0c23a9efed98eafd45313ed683bd0f74fc440dd
dihparaguai <diego.paraguai.dp@gmail.com>
2026-04-24 19:58:28 -0300

    refactor: update Airflow DAG to run modularized jobs functions

--- dags/weather_etl_dag.py
+++ dags/weather_etl_dag.py
@@ -5,6 +5,7 @@ from pendulum import today
 from loguru import logger
 import sys
 
+# Adiciona o path raiz ao PYTHONPATH para que o Airflow consiga importar o código
 sys.path.insert(0, "/opt/airflow")
 
 logger.remove()  # Remove o handler padrão do Loguru
@@ -13,6 +14,16 @@ logger.add(sys.stdout, level="DEBUG")  # Adiciona um handler explícito para std
 from src.jobs.extract import run_extract
 from src.jobs.transform import run_transform
 from src.jobs.load import run_load
+
+cities_names_list = [
+    'Sao Paulo,BR', 'Rio de Janeiro,BR', 'Belo Horizonte,BR', 'Curitiba,BR', 
+    'Porto Alegre,BR', 'Recife,BR', 'Salvador,BR', 'Fortaleza,BR', 
+    'Brasilia,BR', 'Manaus,BR', 'Belem,BR', 'Goiania,BR', 
+    'Campo Grande,BR', 'Florianopolis,BR', 'Vitoria,BR', 'Sao Luis,BR', 
+    'Teresina,BR', 'Natal,BR', 'Joao Pessoa,BR', 'Maceio,BR',
+    'Cuiaba,BR', 'Porto Velho,BR', 'Boa Vista,BR', 'Aracaju,BR',
+    'Palmas,BR', 'Macapa,BR', 'Rio Branco,BR'
+]
 
 default_args = {
     'owner': 'admin', # Proprietário do job
@@ -24,8 +35,8 @@ default_args = {
     dag_id='weather_pipeline_etl', # Identificador único da dag
     default_args=default_args,
     schedule='0 6 * * *', # Intervalo de execução = minuto 0, hora 6, todos os dias do mês, todos os meses, todos os dias da semana
-    start_date=today("America/Sao_Paulo").subtract(days=30), # Data inicio = 30 dias atrás
-    catchup=True, # Se True, executa todas as dags entre a data de início e a data atual
+    start_date=today("America/Sao_Paulo").subtract(days=0), # Data de início da DAG
+    catchup=False, # Se True, executa todas as dags entre a data de início e a data atual
 )
 def weather_etl_pipeline():
     """
@@ -34,23 +45,20 @@ def weather_etl_pipeline():
     """
     
     @task
-    def extract_task() -> str:
+    def extract_task() -> None:
         context_date = get_current_context()['logical_date'] # Usa a data lógica da execução
-        return run_extract(city_name="Sao Paulo,BR", target_date=context_date)
+        run_extract(cities_names_list=cities_names_list, bronze_folder_path='./data/bronze', target_date=context_date)
 
     @task
-    def transform_task(bronze_path: str) -> str:
-        return run_transform(bronze_path)
+    def transform_task() -> None:
+        run_transform(bronze_folder_path='./data/bronze', silver_folder_path='./data/silver')
 
     @task
-    def load_task(silver_path: str):
-        run_load(silver_path)
+    def load_task() -> None:
+        run_load(silver_folder_path='./data/silver', table_name='tb_weather_data')
 
     # Encadeamento de Execução e Dependências (Extract >> Transform >> Load)
-    extract = extract_task()
-    transform = transform_task(extract)
-    load = load_task(transform)
+    extract_task() >> transform_task() >> load_task()
 
-    extract >> transform >> load
-
+# Instancia a DAG e a disponibiliza para o Airflow
 weather_etl_pipeline()
--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-import os, sys
-from loguru import logger
-from src.jobs.extract import run_extract
-from src.jobs.transform import run_transform
-from src.jobs.load import run_load
-from datetime import date
+# import os, sys
+# from loguru import logger
+# from src.jobs.extract import run_extract
+# from src.jobs.transform import run_transform
+# from src.jobs.load import run_load
+# from datetime import date
 
-# logger.remove()
-# logger.add(sys.stdout, level="INFO")
+# # logger.remove()
+# # logger.add(sys.stdout, level="INFO")
 
-def main():
-    logger.info("=== Iniciando Pipeline ETL ===")
+# def main():
+#     logger.info("=== Iniciando Pipeline ETL ===")
     
-    cities_names_list = [
-        'Sao Paulo,BR', 'Rio de Janeiro,BR', 'Belo Horizonte,BR', 'Curitiba,BR', 
-        'Porto Alegre,BR', 'Recife,BR', 'Salvador,BR', 'Fortaleza,BR', 
-        'Brasilia,BR', 'Manaus,BR', 'Belem,BR', 'Goiania,BR', 
-        'Campo Grande,BR', 'Florianopolis,BR', 'Vitoria,BR', 'Sao Luis,BR', 
-        'Teresina,BR', 'Natal,BR', 'Joao Pessoa,BR', 'Maceio,BR',
-        'Cuiaba,BR', 'Porto Velho,BR', 'Boa Vista,BR', 'Aracaju,BR',
-        'Palmas,BR', 'Macapa,BR', 'Rio Branco,BR'
-    ]
+#     cities_names_list = [
+#         'Sao Paulo,BR', 'Rio de Janeiro,BR', 'Belo Horizonte,BR', 'Curitiba,BR', 
+#         'Porto Alegre,BR', 'Recife,BR', 'Salvador,BR', 'Fortaleza,BR', 
+#         'Brasilia,BR', 'Manaus,BR', 'Belem,BR', 'Goiania,BR', 
+#         'Campo Grande,BR', 'Florianopolis,BR', 'Vitoria,BR', 'Sao Luis,BR', 
+#         'Teresina,BR', 'Natal,BR', 'Joao Pessoa,BR', 'Maceio,BR',
+#         'Cuiaba,BR', 'Porto Velho,BR', 'Boa Vista,BR', 'Aracaju,BR',
+#         'Palmas,BR', 'Macapa,BR', 'Rio Branco,BR'
+#     ]
 
-    target_date = date.today()
+#     target_date = date.today()
 
-    # Etapa 1: Extração (Extract)
-    bronze_folder_path = run_extract(cities_names_list=cities_names_list, bronze_folder_path='./data/bronze', target_date=target_date)
-    logger.info(f"Extração Bronze concluída.")
+#     # Etapa 1: Extração (Extract)
+#     run_extract(cities_names_list=cities_names_list, bronze_folder_path='./data/bronze', target_date=target_date)
+#     logger.info("Extração Bronze concluída.")
     
-    # Etapa 2: Transformação (Transform)
-    run_transform(bronze_folder_path='./data/bronze', silver_folder_path='./data/silver')
-    logger.info("Transformação Silver concluída.")
+#     # Etapa 2: Transformação (Transform)
+#     run_transform(bronze_folder_path='./data/bronze', silver_folder_path='./data/silver')
+#     logger.info("Transformação Silver concluída.")
     
-    # Etapa 3: Carga (Load)
-    run_load(silver_folder_path='./data/silver', table_name='tb_weather_data')
-    logger.info("Carga Gold concluída.")
+#     # Etapa 3: Carga (Load)
+#     run_load(silver_folder_path='./data/silver', table_name='tb_weather_data')
+#     logger.info("Carga Gold concluída.")
     
-    logger.info("=== Pipeline Finalizado ===")
+#     logger.info("=== Pipeline Finalizado ===")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
